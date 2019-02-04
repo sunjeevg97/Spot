5f446af9afbaba8c8e64485a8ac435c1c6b4da2c
Sunjeev Guru <sunjeevgururangan@gmail.com>
2019-02-04 11:48:39 -0500

    Initial Build Setup

---
+++ Spot/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spot
+//
+//  Created by Sunjeev Gururangan on 2/4/19.
+//  Copyright © 2019 comp523. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
