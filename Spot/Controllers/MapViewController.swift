69540bb371c2058643a460c1bd4a624cda4305c5
Victoria Elaine Maxwell <vemm97@compmac003.cs.unc.edu>
2019-02-15 15:13:55 -0500

    tab bar with tab bar icons

---
+++ Spot/Controllers/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  Spot
+//
+//  Created by Victoria Elaine Maxwell on 2/15/19.
+//  Copyright © 2019 comp523. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
