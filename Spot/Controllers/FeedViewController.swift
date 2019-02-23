99d313613a536902f6e5e068cd950b7d8930b989
vatsalparikh1 <vatsal@email.unc.edu>
2019-02-23 10:26:34 -0500

    started feed view

--- Spot/Controllers/FeedViewController.swift
+++ Spot/Controllers/FeedViewController.swift
@@ -9,23 +9,43 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        
         self.navigationItem.titleView = UIImageView(image: UIImage(named: "Signuplogo1x.png"))
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+        
+        
+//        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.gray
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+}
+
