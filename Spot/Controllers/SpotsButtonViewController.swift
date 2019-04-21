799ab9b93447187c7d9084f5e485da1e1d712e6a
nbade97 <nishit@192.168.0.104>
2019-04-21 16:24:47 -0400

    editProfile checker rules

--- Spot/Controllers/SpotsButtonViewController.swift
+++ Spot/Controllers/SpotsButtonViewController.swift
@@ -12,7 +12,7 @@ class SpotsButtonViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         
         // Do any additional setup after loading the view.
     }
